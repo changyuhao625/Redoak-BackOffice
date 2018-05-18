44b961c49d7c4f62f91854537784c3cdd8cb2a25
CoulsonChen <a0936792910@gmail.com>
2018-05-18 16:36:56 +0800

    Coulson / 加入多國語系
    安裝nuget → Localization.AspNetCore.TagHelpers
    設定Startup.cs的Configure方法時，UseRequestLocalization要放在UseMvc前面
    Middleware中需移除AcceptLanguageHeaderRequestCultureProvider後預設的語系才能正常運作

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,14 +2,27 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Redoak.Backoffice.Models;
+using Microsoft.Extensions.Localization;
+using Redoak.Backoffice.Middlewares;
 
 namespace Redoak.Backoffice.Controllers
 {
     [Authorize]
+    [MiddlewareFilter(typeof(CultureMiddleware))]
     public class HomeController : Controller
     {
+        private readonly IStringLocalizer<SharedResource> _localizer;
+
+        public HomeController(IStringLocalizer<SharedResource> localizer)
+        {
+            _localizer = localizer;
+        }
+
         public IActionResult Index()
         {
+            // 測試程式碼 TODO：測試完移除
+            var a = _localizer["SystemTitle"];
+
             return View();
         }
     }
---
+++ Middlewares/CultureMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Builder;
+
+namespace Redoak.Backoffice.Middlewares
+{
+    public class CultureMiddleware
+    {
+        private static readonly List<CultureInfo> _supportedCultures = new List<CultureInfo>()
+        {
+            new CultureInfo("en-US"),
+            new CultureInfo("zh-TW")
+        };
+
+        private static readonly RequestLocalizationOptions _localizationOptions = new RequestLocalizationOptions()
+        {
+            DefaultRequestCulture = new RequestCulture(_supportedCultures.First()),
+            SupportedCultures = _supportedCultures,
+            SupportedUICultures = _supportedCultures,
+            RequestCultureProviders = new List<IRequestCultureProvider>()
+            {
+                new QueryStringRequestCultureProvider(),
+                new CookieRequestCultureProvider()
+            }
+        };
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseRequestLocalization(_localizationOptions);
+        }
+    }
+}
